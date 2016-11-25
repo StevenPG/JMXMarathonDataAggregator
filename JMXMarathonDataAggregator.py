547c573fddfb5851905c0df96ec00c88b1481ffd
Steven Gantz <Steven.Gantz@qvc.com>
2016-11-25 10:32:07 -0500

    Reformatted comments

--- JMXMarathonDataAggregator.py
+++ JMXMarathonDataAggregator.py
@@ -17,15 +17,15 @@ The app will then retrieve the data from every available machine:port/metrics
 endpoint and port them to a preset list of ports.
 
 Usage: python3 JMXMarathonDataAggregator.py <marathonurl> <appid> \
-    <initialport> <totalports>
+    <initialport> <totalScaledInstances>
     OR
 Usage: python3 JMXMarathonDataAggregator.py <marathonurl> \
     <appid> <list_of_ports>
 
-marathonurl:   Full URL of marathon instance whose API will be queried
-appid:         Full Appid to use within the REST Api calls
-initialport:   Which port to start with (ex. 4010, 4011, 40<totalports>)
-totalports:    Total number of ports to expose
+marathonurl:             Full URL of marathon instance whose API will be queried
+appid:                   Full Appid to use within the REST Api calls
+initialport:             Which port to start with (ex. 4010, 4011, 40<totalports>)
+totalScaledInstances:    Total number of ports to expose based on max number of scaled instances
 # TODO - list_of_ports: comma seperated list with exact ports to expose
 # TODO - figure out how to handle multiple ports inside each container - For now use first one in portlist
 # TODO - Can also be read from a JMXAggregator.properties file.
@@ -68,7 +68,7 @@ client = MarathonRestService(inputArgs.marathonURL, inputArgs.appid) # build mar
 server = ServerHandler(inputArgs, client)                            # build initial server handler
 server.startSocketServers()                                          # Kick off socket servers
 
-""" LICENSE BELOW
+"""
 
 MIT License
 
